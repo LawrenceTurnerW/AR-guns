761d76cfd48fae00bd073b98a2958b987429e519
LawrenceTurnerW <watawata.takatan@gmail.com>
2023-11-24 02:57:35 +0900

    リロードできる距離を調節

--- Assets/MyAssets/reload.cs
+++ Assets/MyAssets/reload.cs
@@ -2,13 +2,18 @@ using UnityEngine;
 
 public class Reload : MonoBehaviour
 {
+    // ここで指定したオブジェクトの位置や角度を基準にする
+    [SerializeField] private GameObject gunPoint;
+
     // 入力ボタンフィールをを作成できる
     [SerializeField] private OVRInput.RawButton inputBtn;
 
     private void Update()
     {
+        Vector3 gunPointPos = gunPoint.transform.position;
+        float dis = Vector3.Distance(gunPointPos, this.transform.position);
         // 指定したボタンが押されたら
-        if (OVRInput.GetDown(inputBtn))
+        if (OVRInput.GetDown(inputBtn) && dis > 0.7)
         {
             Gun gun; //呼ぶスクリプトにあだなつける
             GameObject obj = GameObject.Find("OVRRuntimeControllerPrefabR");
